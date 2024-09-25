e2ade222e0c30307fb5064b08c781d9340f00f70
AbiramiS18 <itsmeabisoundar@gmail.com>
2024-09-25 12:29:05 +0530

    Files Added

---
+++ SQL-4/Schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Q4data
+	(id int(5),
+     name varchar(15),
+     location varchar(15),
+     primary key(id));
+     
+INSERT INTO Q4data VALUES
+	(1,null,null),
+    (2,"David",null),
+    (3,null,"London"),
+    (4,null, null),
+    (5,"David",null);
---
+++ SQL-4/Solution.sql
@@ -0,0 +1,5 @@
+SELECT MIN(id), MIN(name),MIN(location)
+FROM Q4data;
+
+SELECT MAX(id), MAX(name),MAX(location)
+FROM Q4data;
